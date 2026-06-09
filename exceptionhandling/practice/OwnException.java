951c95d9054e3c3c1ebfb81bda18dcef30023cb5
0-ashwin-nair-0 <ashwin.nair0309@gmail.com>
2026-06-09 10:06:44 +0530

    Own Exception

---
+++ exceptionhandling/practice/OwnException.java
@@ -0,0 +1,21 @@
+package com.exceptionhandling.practice;
+//Creating our own Exception
+//We have to extend Exception class
+class AshwinException extends Exception{
+    public AshwinException(String str){
+        super(str);//this helps in passing the value of string to the parent class
+    }
+}
+public class OwnException {
+    public static void main(String[] args) {
+        String name = "Ashwin";
+        try{
+            if(name.equals("Ashwin")){
+                throw new AshwinException("Ashwin is sleeping");
+            }
+            System.out.println(name+" hieee!!");
+        }catch (AshwinException e){
+            System.out.println("Please don't call Ashwin\n "+e.getMessage());
+        }
+    }
+}
